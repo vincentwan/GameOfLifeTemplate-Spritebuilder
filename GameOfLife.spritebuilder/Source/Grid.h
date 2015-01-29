6e962464d9555c9badd35062cce6c9877709dc95
vincentwan <xinwan1992@gmail.com>
2015-01-28 22:07:10 -0800

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.h
+++ GameOfLife.spritebuilder/Source/Grid.h
@@ -10,6 +10,12 @@
 
 @interface Grid : CCSprite
 
+@property (nonatomic, assign) NSMutableArray *_gridArray;
+@property (nonatomic, assign) float _cellWidth;
+@property (nonatomic, assign) float _cellHeight;
+@property (nonatomic, assign) int _generation;
+@property (nonatomic, assign) int _totalAlive;
+
 - (void)evolveStep;
 - (void)countNeighbors;
 - (void)updateCreatures;
